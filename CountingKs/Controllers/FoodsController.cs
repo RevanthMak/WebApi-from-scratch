1f8f71665e3beae516873ecdddde28af2f67c8bb
revanthkumar.makkena <k.revanth177@gmail.com>
2017-02-15 08:24:32 -0600

    Add project files.

---
+++ CountingKs/Controllers/FoodsController.cs
@@ -0,0 +1,25 @@
+﻿using CountingKs.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CountingKs.Controllers
+{
+    public class FoodsController : ApiController
+    {
+        public object Get()
+        {
+            var repo = new CountingKsRepository(new CountingKsContext());
+
+            var results = repo.GetAllFoods()
+                              .OrderBy(f => f.Description)
+                              .Take(25)
+                              .ToList();
+            return results;
+        }
+
+    }
+}
