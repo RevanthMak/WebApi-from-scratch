9aafcc24793b90fdec93fe42600a1d1c3ee8682f
revanthkumar.makkena <k.revanth177@gmail.com>
2017-02-18 17:41:46 -0600

    Implemented Custom Routing

--- CountingKs/App_Start/WebApiConfig.cs
+++ CountingKs/App_Start/WebApiConfig.cs
@@ -1,23 +1,28 @@
 ﻿using Newtonsoft.Json.Serialization;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace CountingKs
 {
-  public static class WebApiConfig
+    public static class WebApiConfig
   {
         private static object jsonMediaTypeResolver;
 
         public static void Register(HttpConfiguration config)
     {
       config.Routes.MapHttpRoute(
-          name: "DefaultApi",
-          routeTemplate: "api/{controller}/{id}",
-          defaults: new { id = RouteParameter.Optional }
+          name: "Food",
+          routeTemplate: "api/nutrition/foods/{id}",
+          defaults: new { Controller ="Foods",  id = RouteParameter.Optional }
       );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
 
             // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
             // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
@@ -27,6 +32,8 @@ namespace CountingKs
             var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().FirstOrDefault();
             jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
-    }
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+        }
   }
 }
